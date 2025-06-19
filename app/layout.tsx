a8479998fa0d18c3e89b55620ffceea43a3cd2f5
Saloni Patel <saloni230301@gmail.com>
2025-06-19 13:08:48 -0400

    added logo and header

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Nourish Resilience ',
+  description: 'Created By Saloni',
+  generator: 'saloni',
 }
 
 export default function RootLayout({
