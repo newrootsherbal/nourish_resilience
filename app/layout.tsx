60483af4aafa298358e82617242728c1fc12384d
Nicolas Desjardins <nick.desjardins@live.com>
2025-08-07 09:40:51 -0400

    meta changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,11 +2,45 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'Nourish Resilience ',
-  description: 'Created By Saloni',
-  generator: 'saloni',
+  title: 'Nourish Resilience - Clinically Informed, Organic Vegan Protein Supplement for Women in Recovery',
+  description: 'Nourish Resilience is a clinically informed, organic vegan protein supplement designed to support women in their recovery journey after serious illness, including breast cancer. This functional nutrition formula combines bioavailable proteins, powerful antioxidants, adaptogenic medicinal mushrooms, fermented botanicals, and gut-supportive prebiotics.',
+  generator: 'Vitazan Professional',
+  keywords: [
+    'Nourish Resilience',
+    'recovery',
+    'organic vegan protein supplement',
+    'breast cancer',
+    'functional nutrition',
+    'clinically informed',
+    'healthy living',
+    'wellness'
+  ],
   icons: {
     icon: '/favicon.jpg', // Path relative to the public directory
+  },
+  openGraph: {
+    title: 'Nourish Resilience - Clinically Informed, Organic Vegan Protein Supplement for Women in Recovery',
+    description: 'Nourish Resilience is a clinically informed, organic vegan protein supplement designed to support women in their recovery journey after serious illness, including breast cancer.',
+    images: [
+      {
+        url: '/3173-Nourish-Resilience-476g.png', // Use actual image path from public folder
+        alt: 'An image representing the Nourish Resilience brand',
+      },
+    ],
+    url: 'https://nourishresilience.ca',
+    type: 'website',
+    siteName: 'Nourish Resilience',
+  },
+  twitter: {
+    title: 'Nourish Resilience - Clinically Informed, Organic Vegan Protein Supplement for Women in Recovery',
+    description: 'Nourish Resilience is a clinically informed, organic vegan protein supplement designed to support women in their recovery journey after serious illness, including breast cancer.',
+    images: [
+      {
+        url: '/3173-Nourish-Resilience-476g.png',
+        alt: 'An image representing the Nourish Resilience brand',
+      },
+    ],
+    card: 'summary_large_image',
   },
 }
 
